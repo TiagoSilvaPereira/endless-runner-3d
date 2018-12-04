f7c490ec68127e12779d9b0d679a0cb042f72b11
Tiago Rodrigues <contato@kingofcode.com.br>
2018-12-04 21:27:51 -0200

    Infinite level by player travelled distance and removing old level tiles

--- src/Level.js
+++ src/Level.js
@@ -4,11 +4,12 @@ class Level {
 
         this.scene = null;
         this.player = null;
-        this.playerSpeed = 15;
 
         this.tileDepth = 10;
         this.holeDepth = 10;
+        this.canGenerateMoreTiles = false;
         this.lastTileType = 'HOLE';
+        this.generatedTilesNumber = 0;
         // It is used to make hole's on the map, increment the position with the hole size
         this.currentTilePositionIncrementer = 0;
 
@@ -35,7 +36,7 @@ class Level {
         var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), this.scene);
         var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), this.scene);
 
-        this.createGround();
+        this.generateGroundTiles();
 
         // Creates the player and set it as camera target
         this.player = new Player();
@@ -60,9 +61,9 @@ class Level {
         return camera;
     }
 
-    createGround() {
+    generateGroundTiles() {
 
-        // We'll use this array to determine the type of ground we will create
+        // We'll use this array to determine the type of ground to create
         let tileTypes = [
             'NORMAL_GROUND',
             'SMALL_GROUND',
@@ -71,9 +72,11 @@ class Level {
             'GROUND_WITH_HIGH_OBSTACLE'
         ];
         
-        // Let's generate the next 100 ground tiles (or holes :D)
-        for(var currentTileNumber = 0; currentTileNumber < 100; currentTileNumber++) {
+        // Let's generate the next 10 ground tiles (or holes :D) - 100 "meters" or tiles
+        for(var generatedTilesNumber = 0; generatedTilesNumber < 10; generatedTilesNumber++) {
             
+            this.generatedTilesNumber++;
+
             // Choose a tyle type randomly
             let randomTileTypeNumber = Math.floor((Math.random() * tileTypes.length));
             let tyleType = tileTypes[randomTileTypeNumber];
@@ -86,35 +89,37 @@ class Level {
             this.lastTileType = tyleType;
 
             if(tyleType == 'NORMAL_GROUND') {
-                this.createNormalGroundTile(currentTileNumber);
+                this.createNormalGroundTile();
             }
 
             if(tyleType == 'SMALL_GROUND') {
-                this.createSmallGroundTile(currentTileNumber);
+                this.createSmallGroundTile();
             }
 
             if(tyleType == 'HOLE') {
-                this.createHoleTile(currentTileNumber);
+                this.createHoleTile();
             }
 
             if(tyleType == 'GROUND_WITH_TOTAL_OBSTACLE') {
-                this.createGroundWithObstacleTile(currentTileNumber);
+                this.generateGroundTilesWithObstacleTile();
             }
 
             if(tyleType == 'GROUND_WITH_HIGH_OBSTACLE') {
-                this.createGroundWithHighObstacleTile(currentTileNumber);
+                this.generateGroundTilesWithHighObstacleTile();
             }
 
         }
 
     }
 
-    createNormalGroundTile(currentTileNumber) {
+    createTile(options) {
 
-        let tile = BABYLON.MeshBuilder.CreateBox("groundTile" + currentTileNumber, {width: 1, height: 1, depth: this.tileDepth}, this.scene);
+        options = options ? options : { width: 1, height: 1, depth: this.tileDepth };
+
+        let tile = BABYLON.MeshBuilder.CreateBox("groundTile" + this.generatedTilesNumber, options, this.scene);
         let tileMaterial = new BABYLON.StandardMaterial("tileMaterial", this.scene);
             
-        tile.position.z = (currentTileNumber * this.tileDepth) + this.currentTilePositionIncrementer;
+        tile.position.z = ((this.generatedTilesNumber - 1) * this.tileDepth);
         tile.position.y = -0.5;
 
         tileMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
@@ -123,102 +128,91 @@ class Level {
         tile.checkCollisions = true;
 
         // Intercaling the ground color
-        if((currentTileNumber % 2) == 0) {
+        if((this.generatedTilesNumber % 2) == 0) {
             tile.material.diffuseColor = new BABYLON.Color3(0.8, 0.3, 0.3);
         }
 
-        this.currentTilePositionIncrementer = 0;
+        return tile;
 
     }
 
-    createSmallGroundTile(currentTileNumber) {
+    createNormalGroundTile() {
+        this.createTile();
+    }
 
-        let tile = BABYLON.MeshBuilder.CreateBox("groundTile" + currentTileNumber, {width: 0.3333, height: 1, depth: this.tileDepth}, this.scene);
-        let tileMaterial = new BABYLON.StandardMaterial("tileMaterial", this.scene);
+    createSmallGroundTile() {
+        let tile = this.createTile({ width: 0.3333, height: 1, depth: this.tileDepth});
         
         // Choose the side to place the ground
         let randomSideChooser = Math.floor((Math.random() * 100) + 1);
 
         tile.position.x = (randomSideChooser <= 50) ? -0.3333 : 0.3333;
-        tile.position.z = (currentTileNumber * this.tileDepth) + this.currentTilePositionIncrementer;
         tile.position.y = -0.5;
-
-        tileMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-        tile.material = tileMaterial.clone();
-        
-        tile.checkCollisions = true;
-        
-        // Intercaling the ground color
-        if((currentTileNumber % 2) == 0) {
-            tile.material.diffuseColor = new BABYLON.Color3(0.8, 0.3, 0.3);
-        }
-
-        this.currentTilePositionIncrementer = 0;
-
     }
 
-    createHoleTile(currentTileNumber) {
+    createHoleTile(tileNumber) {
         //this.currentTilePositionIncrementer = this.holeDepth;
     }
 
-    createGroundWithObstacleTile(currentTileNumber) {
+    generateGroundTilesWithObstacleTile() {
 
-        let tile = BABYLON.MeshBuilder.CreateBox("groundTile" + currentTileNumber, {width: 1, height: 1, depth: this.tileDepth}, this.scene);
-        let obstacle = BABYLON.MeshBuilder.CreateBox("obstacleTile" + currentTileNumber, {width: 1, height: 0.25, depth: 0.25}, this.scene);
-        
-        let tileMaterial = new BABYLON.StandardMaterial("tileMaterial", this.scene);
-            
-        tile.position.z = (currentTileNumber * this.tileDepth) + this.currentTilePositionIncrementer;
-        tile.position.y = -0.5;
+        let tile = this.createTile();
+        let obstacle = BABYLON.MeshBuilder.CreateBox("obstacleTile" + this.generatedTilesNumber, {width: 1, height: 0.25, depth: 0.25}, this.scene);
 
         obstacle.position.z = tile.position.z;
         obstacle.position.y = 0.125;
 
-        tile.checkCollisions = true;
-
-        tileMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-        tile.material = tileMaterial.clone();
-        
-        // Intercaling the ground color
-        if((currentTileNumber % 2) == 0) {
-            tile.material.diffuseColor = new BABYLON.Color3(0.8, 0.3, 0.3);
-        }
-
-        this.currentTilePositionIncrementer = 0;
-
     }
 
-    createGroundWithHighObstacleTile(currentTileNumber) {
+    generateGroundTilesWithHighObstacleTile(tileNumber) {
 
-        let tile = BABYLON.MeshBuilder.CreateBox("groundTile" + currentTileNumber, {width: 1, height: 1, depth: this.tileDepth}, this.scene);
-        let obstacle = BABYLON.MeshBuilder.CreateBox("obstacleTile" + currentTileNumber, {width: 2, height: 2, depth: 0.25}, this.scene);
-        
-        let tileMaterial = new BABYLON.StandardMaterial("tileMaterial", this.scene);
-            
-        tile.position.z = (currentTileNumber * this.tileDepth) + this.currentTilePositionIncrementer;
-        tile.position.y = -0.5;
+        let tile = this.createTile(tileNumber);
+        let obstacle = BABYLON.MeshBuilder.CreateBox("obstacleTile" + this.generatedTilesNumber, {width: 2, height: 2, depth: 0.25}, this.scene);
 
         obstacle.position.z = tile.position.z;
         obstacle.position.y = 1.5;
 
-        tileMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-        tile.material = tileMaterial.clone();
-
-        tile.checkCollisions = true;
-        
-        // Intercaling the ground color
-        if((currentTileNumber % 2) == 0) {
-            tile.material.diffuseColor = new BABYLON.Color3(0.8, 0.3, 0.3);
-        }
-
-        this.currentTilePositionIncrementer = 0;
-
     }
 
     beforeRender() {
-        
         this.player.move();
 
+        // If player travelled distance was reseted (is lower than 70),
+        // then I can dispose old tiles and allow to generate more ground tiles
+        if(this.player.getTravelledDistance() < 10 && !this.canGenerateMoreTiles) {
+            this.disposeOldTiles();
+            this.canGenerateMoreTiles = true;
+        }
+
+        // If player has travelled more 70 "meters", needs to generate more ground tiles,
+        // and block more tiles generation until that the distance was reseted
+        if(this.player.getTravelledDistance() >= 70 && this.canGenerateMoreTiles) {
+            this.generateGroundTiles();
+            this.canGenerateMoreTiles = false;
+        }
+
+    }
+
+    /**
+     * Dispose old tiles and obstacles (last 10 unused tiles and their obstacles)
+     */
+    disposeOldTiles() {
+        let fromTile = this.generatedTilesNumber - 20,
+            toTile = this.generatedTilesNumber - 10,
+            meshToDispose = null;
+        
+        for(; fromTile < toTile; fromTile++) {
+            let tileName = 'groundTile' + fromTile,
+                obstacleName = 'obstacleTile' + fromTile;
+            
+            if(meshToDispose = this.scene.getMeshByName(tileName)) {
+                meshToDispose.dispose();
+            }
+
+            if(meshToDispose = this.scene.getMeshByName(obstacleName)) {
+                meshToDispose.dispose();
+            }
+        }
     }
     
 }
--- src/Player.js
+++ src/Player.js
@@ -3,7 +3,20 @@ class Player {
     constructor(scene) {
         
         this.scene = scene;
+        
+        this.speed = 100;
 
+        this.gravity = -12;
+        
+        /**
+         * Used to store the travelled distance and calculate where to generate more level tiles
+         * and to give points to the player
+         * The travelledDistance will reset each 100 "meters". When travelledDistance is equal to 70
+         * the Level will generate more tiles
+         */
+        this.travelledDistance = 0;
+        this.totalTravelledDistance = 0;
+        
         /**
          * Method to set an ellipsoid (for collision) based on boundingbox size 
          */ 
@@ -34,36 +47,57 @@ class Player {
     }
 
     move() {
+
         let elapsedTime = (GAME.engine.getDeltaTime() / 1000);
 
-        //this.mesh.position.z += this.meshSpeed * elapsedTime;
-        this.mesh.moveWithCollisions(new BABYLON.Vector3(0, -0.09, this.meshSpeed * elapsedTime));
+        this.mesh.moveWithCollisions(new BABYLON.Vector3(
+            0, 
+            //this.gravity / 100, 
+            0,
+            this.speed * elapsedTime
+        ));
+        
+        this.calculateTravelledDistance(elapsedTime);
 
         if(GAME.keys.up && this.mesh.position.y < 4) {
-            this.mesh.position.y += (this.meshSpeed) * elapsedTime;
+            this.mesh.position.y += (this.speed) * elapsedTime;
         }
 
         if(GAME.keys.left) {
-            this.mesh.position.x -= (this.meshSpeed / 3) * elapsedTime;
+            this.mesh.position.x -= (this.speed / 3) * elapsedTime;
         }
 
         if(GAME.keys.right) {
-            this.mesh.position.x += (this.meshSpeed / 3) * elapsedTime;
+            this.mesh.position.x += (this.speed / 3) * elapsedTime;
         }
         
         if(GAME.keys.down) {
             this.mesh.scaling.y = 0.5;
-            this.meshSpeed = 20;
-            //this.mesh.position.y = 0.125;
+            this.mesh.setEllipsoidPerBoundingBox();
+            this.speed = 20;
         } else {
-            this.meshSpeed = 15;
+            this.speed = 15;
+            this.mesh.setEllipsoidPerBoundingBox();
             this.mesh.scaling.y = 1;
-            //this.mesh.position.y = 0.25;
         }
+
     }
 
     getMesh() {
         return this.mesh;
+    }
+
+    calculateTravelledDistance(elapsedTime) {
+        if(this.travelledDistance >= 100) {
+            this.travelledDistance = 0;
+        }
+
+        this.travelledDistance += this.speed * elapsedTime;
+        this.totalTravelledDistance += this.speed * elapsedTime;
+    }
+
+    getTravelledDistance() {
+        return this.travelledDistance;
     }
 
 }
