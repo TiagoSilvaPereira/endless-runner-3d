f59ddd15a5f0ec7edb799df5dc2e703bdebbfcf4
Tiago Rodrigues <contato@kingofcode.com.br>
2018-12-13 00:05:37 -0200

    Returning to Arc Camera, but without canvas control to prevent camera movements

--- src/levels/RunnerLevel.js
+++ src/levels/RunnerLevel.js
@@ -42,7 +42,6 @@ class RunnerLevel extends Level {
 
         // This attaches the camera to the canvas
         this.scene.activeCamera = camera;
-        camera.attachControl(GAME.canvas, true);
 
         // Add lights to the scene
         var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 10, 0), this.scene);
@@ -112,24 +111,6 @@ class RunnerLevel extends Level {
         camera.setPosition(new BABYLON.Vector3(0, 1, -2));
         camera.radius = 2;
 
-        return camera;
-    }
-
-    createFollowCamera() {
-        let camera = new BABYLON.FollowCamera("followCamera", new BABYLON.Vector3(0, 10, -10), this.scene);
-    
-        // The goal distance of camera from target
-        camera.radius = 2;
-        // The goal height of camera above local origin (centre) of target
-        camera.heightOffset = 1;
-        // The goal rotation of camera around local origin (centre) of target in x y plane
-        // camera.rotationOffset = 180;
-        camera.rotationOffset = 180;
-        // Acceleration of camera in moving from current to goal position
-        camera.cameraAcceleration = 0.05;
-        // The speed at which acceleration is halted
-        camera.maxCameraSpeed = 10;
-    
         return camera;
     }
 
