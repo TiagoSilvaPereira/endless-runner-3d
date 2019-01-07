0dca0c1163008833b09e3ff5dac51aca1a230269
Tiago Rodrigues <contato@kingofcode.com.br>
2019-01-07 20:49:05 -0200

    Creating the assets manager

--- src/AssetsDatabase.js
+++ src/AssetsDatabase.js
@@ -1,3 +1,40 @@
 class AssetsDatabase {
     
+    constructor(scene) {
+
+        this.scene = scene;
+
+        this.meshes = [];
+        this.sounds = [];
+
+        this.manager = new BABYLON.AssetsManager(this.scene);
+
+    }
+
+    addMesh() {
+
+    }
+
+    addSound(name, file, options) {
+        let fileTask = this.manager.addBinaryFileTask(name + '__Task', file);
+        
+        this.sounds[name] = {};
+
+        fileTask.onSuccess = function (task) {
+            this.sounds[name] = new BABYLON.Sound("Violons18", task.data, scene, soundReady, { loop: true });
+        }
+    }
+
+    addMusic() {
+        
+    }
+
+    getMesh(name) {
+        return this.meshes[name];     
+    }
+
+    getSound(name) {
+        return this.sounds[name];
+    }
+
 }
