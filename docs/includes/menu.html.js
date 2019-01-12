6bc2056744799f610c789daef59863193da571e4
Tiago Rodrigues <contato@kingofcode.com.br>
2019-01-12 19:10:35 -0200

    Adding running documentation

--- docs/includes/menu.html.js
+++ docs/includes/menu.html.js
@@ -4,7 +4,8 @@ document.write('\
 \
     <ul class="pure-menu-list">\
         <li class="pure-menu-item"><a href="' + menuContext + 'index.html" class="pure-menu-link">Introduction</a></li>\
-        <li class="pure-menu-item"><a href="' + menuContext + 'pages/running.html" class="pure-menu-link">Running</a></li>\
+        <li class="pure-menu-item"><a href="' + menuContext + 'pages/running.html" class="pure-menu-link">Running the game</a></li>\
+        <li class="pure-menu-item"><a href="' + menuContext + 'pages/publishing.html" class="pure-menu-link">Publishing the game</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/changing.html" class="pure-menu-link">Changing basic game options</a></li>\
         <li class="pure-menu-item pure-menu-with-divider"><div class="pure-menu-divider"></div></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/about_code.html" class="pure-menu-link">About the Code</a></li>\
@@ -14,7 +15,7 @@ document.write('\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/ui_class.html" class="pure-menu-link">The UI Class</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/log_class.html" class="pure-menu-link">The Log Class</a></li>\
         <li class="pure-menu-item pure-menu-with-divider"><div class="pure-menu-divider"></div></li>\
-        <li class="pure-menu-item"><a href="' + menuContext + 'pages/licenses.html" class="pure-menu-link">Licenses [Important]</a></li>\
+        <li class="pure-menu-item"><a href="' + menuContext + 'pages/licenses.html" class="pure-menu-link">Licenses <div class="important"></div></a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/more.html" class="pure-menu-link">Wants more game sources?</a></li>\
         \
     </ul>\
