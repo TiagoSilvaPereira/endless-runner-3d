a1bcaaecf4346037bfe82354cbb3a9cff32f532d
Tiago Rodrigues <contato@kingofcode.com.br>
2019-01-12 18:16:18 -0200

    UI class documentation

--- docs/includes/menu.html.js
+++ docs/includes/menu.html.js
@@ -6,8 +6,14 @@ document.write('\
         <li class="pure-menu-item"><a href="' + menuContext + 'index.html" class="pure-menu-link">Introduction</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/running.html" class="pure-menu-link">Running</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/changing.html" class="pure-menu-link">Changing basic game options</a></li>\
+        <li class="pure-menu-item pure-menu-with-divider"><div class="pure-menu-divider"></div></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/about_code.html" class="pure-menu-link">About the Code</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/modifying.html" class="pure-menu-link">Modifying the Code</a></li>\
+        <li class="pure-menu-item"><a href="' + menuContext + 'pages/level_class.html" class="pure-menu-link">The Level Class</a></li>\
+        <li class="pure-menu-item"><a href="' + menuContext + 'pages/assets_class.html" class="pure-menu-link">The AssetsDatabase Class</a></li>\
+        <li class="pure-menu-item"><a href="' + menuContext + 'pages/ui_class.html" class="pure-menu-link">The UI Class</a></li>\
+        <li class="pure-menu-item"><a href="' + menuContext + 'pages/log_class.html" class="pure-menu-link">The Log Class</a></li>\
+        <li class="pure-menu-item pure-menu-with-divider"><div class="pure-menu-divider"></div></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/licenses.html" class="pure-menu-link">Licenses [Important]</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/more.html" class="pure-menu-link">Wants more game sources?</a></li>\
         \
