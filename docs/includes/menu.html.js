bea1ac4ec0f7bf3b5a63d7f9baa9367fca617e3c
Tiago Rodrigues <contato@kingofcode.com.br>
2019-01-15 16:49:21 -0200

    Documentation improvements

--- docs/includes/menu.html.js
+++ docs/includes/menu.html.js
@@ -11,7 +11,6 @@ document.write('\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/about_code.html" class="pure-menu-link">About the Code</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/modifying.html" class="pure-menu-link">Modifying the Code</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/level_class.html" class="pure-menu-link">The Level Class</a></li>\
-        <li class="pure-menu-item"><a href="' + menuContext + 'pages/assets_class.html" class="pure-menu-link">The AssetsDatabase Class</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/ui_class.html" class="pure-menu-link">The UI Class</a></li>\
         <li class="pure-menu-item"><a href="' + menuContext + 'pages/log_class.html" class="pure-menu-link">The Log Class</a></li>\
         <li class="pure-menu-item pure-menu-with-divider"><div class="pure-menu-divider"></div></li>\
